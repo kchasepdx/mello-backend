3e97869e675135d56c9ef4ceca4d0936207c7816
kchasepdx <kchasepdx@gmail.com>
2021-08-23 11:00:36 -0700

    change access control headers in server

--- server.js
+++ server.js
@@ -29,6 +29,17 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/checkout", checkoutRoute);
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
