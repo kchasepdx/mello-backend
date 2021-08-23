be30d29da5d5df0f277f04120a676412fea09206
kchasepdx <kchasepdx@gmail.com>
2021-08-23 11:20:00 -0700

    install mongodb

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import userRoute from "./backend/routes/userRoute.js";
 import productRoute from "./backend/routes/productRoute.js";
 import checkoutRoute from "./backend/routes/checkoutRoute.js";
 const uri = process.env.MONGODB_URI;
+const { MongoClient } = require("mongodb");
 
 dotenv.config();
 
