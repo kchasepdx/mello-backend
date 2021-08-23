3f94642c286b4b995df9cdc9a59e84d05ea11cb1
kchasepdx <kchasepdx@gmail.com>
2021-08-23 13:01:53 -0700

    change bufferCommands in product schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name: String,
-  category: String,
-  image: String,
-  price: Number,
-  brand: String,
-  countInStock: Number,
-});
+const productSchema = new mongoose.Schema(
+  {
+    name: String,
+    category: String,
+    image: String,
+    price: Number,
+    brand: String,
+    countInStock: Number,
+  },
+  { bufferCommands: false, autoCreate: false }
+);
 const Product = mongoose.model("Product", productSchema);
 
 export default Product;
