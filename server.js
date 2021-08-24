8b07e449dcbca03f4c16b1779b1be27dfb833539
kchasepdx <kchasepdx@gmail.com>
2021-08-23 20:52:51 -0700

    refactor server.js

--- server.js
+++ server.js
@@ -4,61 +4,40 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 import Product from "./backend/models/productModel.js";
-// import userRoute from "./backend/routes/userRoute.js";
-// import productRoute from "./backend/routes/productRoute.js";
-// import checkoutRoute from "./backend/routes/checkoutRoute.js";
+import userRoute from "./backend/routes/userRoute.js";
+import productRoute from "./backend/routes/productRoute.js";
+import checkoutRoute from "./backend/routes/checkoutRoute.js";
 const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/mello";
-const app = express();
 
 dotenv.config();
-mongoose.connect(uri, {
-  useCreateIndex: true,
+mongoose.connect("mongodb://localhost:27017/mello", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
-  family: 4,
 });
-const db = mongoose.connection;
-console.log("mongoose connected Database name:" + db.name);
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  console.log(db.name);
-  console.log("host", db.host);
-});
+mongoose.set("useCreateIndex", true);
 
-app.use(cors({ origin: "http://localhost:3000" }));
+const app = express();
+
+app.use(
+  cors({
+    origin: "https://mellostore.herokuapp.com/",
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use("/api/kittens", kittyRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/checkout", checkoutRoute);
 
-app.get("/editproducts", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    if (products) {
-      res.send(products);
-    }
-  } catch (error) {
-    res.send({ message: "could not get products, " + error });
-  }
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("mongoose connected");
+  console.log(db.name);
+  console.log("host", db.host);
 });
-
-// const db = mongoose.connection;
-// console.log("mongoose connected Database name:" + db.name);
-// db.on("error", console.error.bind(console, "connection error:"));
-// db.once("open", function () {
-//   console.log(db.name);
-//   console.log("host", db.host);
-// // });
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(morgan("tiny"));
-// app.use("/api/users", userRoute);
-// app.use("/api/products", productRoute);
-// app.use("/api/checkout", checkoutRoute);
 
 app.listen(process.env.PORT, () => {
   console.log("server started", process.env.PORT || 5000);
